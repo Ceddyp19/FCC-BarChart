37ba4a7b14269eb0c540fa2b833d18fb4f3c4e5d
Ceddyp19 <ceddyp117@gmail.com>
2021-04-20 13:08:49 -0500

    x-scale working

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
         h = 500,
         padding = 30;
 
-    const svg = d3.select('#chart')
+    const svg = d3.select('body')
         .append('svg')
         .attr('width', w)
         .attr('height', h)
@@ -18,35 +18,48 @@ document.addEventListener('DOMContentLoaded', function () {
     req.onload = function () {
         const json = JSON.parse(req.responseText);
         const dataset = json.data
-        // console.log(dataset[dataset.length - 1][1])
+        // console.log(dataset[dataset.length - 1][0])
+        console.log(dataset)
+        // ***************************************************
+        //REFINE DATA AND CREATE SCALES
+        // ==========X-Scale===========
+        let dates = dataset.map(function (d) {
+            // return d[0].substr(0, 4)
+            return new Date(d[0]);
+        }),
+            // xMax = d3.max(dates),
+            xMin = d3.min(dates);
+        var xMax = new Date(d3.max(dates));
+        //add a few more months to max so x-axis ends a little after last bar graph--aesthetic purposes basically       
+        xMax.setMonth(xMax.getMonth() + 3);
 
+        const xScale = d3.scaleTime()
+            .domain([xMin, xMax])
+            .range([padding, w - padding]);
 
-        //create scales
-        // const xScale = d3.scaleLinear()
-        // .domain([dataset[0][0], dataset[dataset.length - 1][0]])
-        // .range(padding, w - padding)
-
-        // var xMax = new Date(d3.max(yearsDate));
-        // xMax.setMonth(xMax.getMonth() + 3);
-        // var xScale = d3
-        //   .scaleTime()
-        //   .domain([d3.min(yearsDate), xMax])
-        //   .range([0, width]);
-
-
+        // ==========X-Scale===========
         const yScale = d3.scaleLinear()
             .domain([0, dataset[dataset.length - 1][1]])
             .range([h, 0])
-
+        // ***************************************************
         // create bars
         svg.selectAll('rect')
             .data(dataset)
             .enter()
             .append('rect')
-            .attr("x", (d, i) => i * 4)
-            .attr("y", (d, i) =>  h - d[1]/40)
+            .attr("x", (d, i) => xScale(dates[i]))
+            // .attr("x", (d, i) => xScale(d[0].substr(0, 4)))
+            .attr("y", (d, i) => h - d[1] / 40)
             .attr("width", 3)
-            .attr("height", (d) => d[1]/40);
+            .attr("height", (d) => d[1] / 40)
+            .attr('class', 'bar')
+
+        // add Axes
+        const xAxis = d3.axisBottom(xScale);
+        svg.append('g')
+            .attr("transform", "translate(0, " + (h - padding) + ")")
+            .call(xAxis)
+            .attr('id', 'x-axis');
     };
 
 
